ec714ecdfd95a0211d1ee48fb14ed69879ecc38d
NewtTheWolf <dominik@spitzli.dev>
2026-08-25 13:42:10 +0200

    chore: drop unused dependencies, update readme, tests and ignores

--- tests/e2e/frontend.e2e.spec.ts
+++ tests/e2e/frontend.e2e.spec.ts
@@ -1,17 +1,9 @@
-import { test, expect, Page } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test.describe('Frontend', () => {
-  let page: Page
-
-  test.beforeAll(async ({ browser }, testInfo) => {
-    const context = await browser.newContext()
-    page = await context.newPage()
-  })
-
   test('can load homepage', async ({ page }) => {
     await page.goto('http://localhost:3000')
-    await expect(page).toHaveTitle(/Payload Website Template/)
-    const heading = page.locator('h1').first()
-    await expect(heading).toHaveText('Payload Website Template')
+    await expect(page).toHaveTitle(/Stall Eichenbruch/)
+    await expect(page.locator('header')).toContainText('Stall Eichenbruch')
   })
 })
